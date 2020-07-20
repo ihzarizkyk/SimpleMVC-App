e9e76e9a2340556f11e05bb94c3315b99879f07b
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-07-20 13:50:03 +0700

    Add files via upload

---
+++ app/controllers/kontak.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Author : Mochammad Ihza Rizky Karim
+*/
+
+class Kontak extends Controller{
+
+	public function index()
+	{
+		echo "Kontak";
+	}
+
+	public function hardware()
+	{
+		echo "This is Hardware";
+	}
+}
